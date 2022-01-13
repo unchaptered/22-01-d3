1fc42b7bd3b281b004f9902bdcbe44e9449f64a2
Unchaptered <workstation19961002@gmail.com>
2022-01-13 22:30:10 +0900

    000 기초 자료

---
+++ src/000 template/customs.js
@@ -0,0 +1,17 @@
+const womanOfCustom=[
+    { target:"10대", gender:"여성", mount:300 },
+    { target:"20대", gender:"여성", mount:4300 },
+    { target:"30대", gender:"여성", mount:6000 },
+    { target:"40대", gender:"여성", mount:3000 },
+    { target:"50대", gender:"여성", mount:2000 },
+    { target:"60대 이상", gender:"여성", mount:800 },
+];
+
+const mansOfCustom=[
+    { target:"10대", gender:"남성", mount:1000 },
+    { target:"20대", gender:"남성", mount:2000 },
+    { target:"30대", gender:"남성", mount:2200 },
+    { target:"40대", gender:"남성", mount:5000 },
+    { target:"50대", gender:"남성", mount:5500 },
+    { target:"60대 이상", gender:"남성", mount:1200 }
+];
